810084fc1fcd0937d5d4166a44ce2f42149cee9d
Coding-Pratham <doshprat@sheridancollege.ca>
2025-01-26 22:43:05 -0500

    Pratham_Doshi_ICE1

--- src/card/CardTrick.java
+++ src/card/CardTrick.java
@@ -10,15 +10,49 @@ package card;
  * for the match to the user's card. To be used as starting code in ICE 1
  * @author srinivsi
  */
+import java.util.Random;
+import java.util.Scanner;
+
 public class CardTrick {
     
-    public static void main(String[] args)
-    {
+    public static void main(String[] args) {
         Card[] magicHand = new Card[7];
-        
-        for (int i=0; i<magicHand.length; i++)
-        {
+        Random random = new Random();
+
+        for (int i = 0; i < magicHand.length; i++) {
             Card c = new Card();
+            c.setValue(random.nextInt(13) + 1);  // Random number between 1-13
+            c.setSuit(Card.SUITS[random.nextInt(4)]);  // Random suit from SUITS array
+            magicHand[i] = c;
+        }
+
+        // Display generated cards for reference (Optional)
+        System.out.println("Magic Hand Cards:");
+        for (Card card : magicHand) {
+            System.out.println(card.getValue() + " of " + card.getSuit());
+        }
+
+        // Prompt the user to pick a card
+        Scanner input = new Scanner(System.in);
+        System.out.println("\nPick a card (1-13): ");
+        int number = input.nextInt();
+        System.out.println("Pick a suit (Hearts, Diamonds, Spades, Clubs): ");
+        String suit = input.next();
+        
+        // Search for the user's card in magicHand
+        boolean found = false;
+        for (Card card : magicHand) {
+            if (card.getValue() == number && card.getSuit().equalsIgnoreCase(suit)) {
+                found = true;
+                break;
+            }
+        }
+
+        if (found) {
+            System.out.println("Congratulations! Your card is in the magic hand.");
+        } else {
+            System.out.println("Sorry, your card is not in the magic hand.");
+        }
             //c.setValue(insert call to random number generator here)
             //c.setSuit(Card.SUITS[insert call to random number between 0-3 here])
         }
@@ -27,6 +61,6 @@ public class CardTrick {
         // and search magicHand here
         //Then report the result here
         // add one luckcard hard code 2,clubs
-    }
-    
 }
+    
+
